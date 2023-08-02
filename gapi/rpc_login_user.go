77b4ad5739257b0cf3694dcf78b3bfbbf70b1da6
vinhhung263 <hungpv263@gmail.com>
2023-08-02 10:09:58 +0700

    config swagger

--- gapi/rpc_login_user.go
+++ gapi/rpc_login_user.go
@@ -7,19 +7,21 @@ import (
 	db "github.com/vinhhung263/simplebank/db/sqlc"
 	"github.com/vinhhung263/simplebank/pb"
 	"github.com/vinhhung263/simplebank/util"
+	"github.com/vinhhung263/simplebank/val"
 
 	// "github.com/vinhhung263/simplebank/val"
 
+	"google.golang.org/genproto/googleapis/rpc/errdetails"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 	"google.golang.org/protobuf/types/known/timestamppb"
 )
 
 func (server *Server) LoginUser(ctx context.Context, req *pb.LoginUserRequest) (*pb.LoginUserResponse, error) {
-	// violations := validateLoginUserRequest(req)
-	// if violations != nil {
-	// 	return nil, invalidArgumentError(violations)
-	// }
+	violations := validateLoginUserRequest(req)
+	if violations != nil {
+		return nil, invalidArgumentError(violations)
+	}
 
 	user, err := server.store.GetUser(ctx, req.GetUsername())
 	if err != nil {
@@ -50,13 +52,13 @@ func (server *Server) LoginUser(ctx context.Context, req *pb.LoginUserRequest) (
 		return nil, status.Errorf(codes.Internal, "failed to create refresh token")
 	}
 
-	// mtdt := server.extractMetadata(ctx)
+	mtdt := server.extractMetadata(ctx)
 	session, err := server.store.CreateSession(ctx, db.CreateSessionParams{
 		ID:           refreshPayload.ID,
 		Username:     user.Username,
 		RefreshToken: refreshToken,
-		UserAgent:    "",
-		ClientIp:     "",
+		UserAgent:    mtdt.UserAgent,
+		ClientIp:     mtdt.ClientIP,
 		IsBlocked:    false,
 		ExpiresAt:    refreshPayload.ExpiredAt,
 	})
@@ -75,14 +77,14 @@ func (server *Server) LoginUser(ctx context.Context, req *pb.LoginUserRequest) (
 	return rsp, nil
 }
 
-// func validateLoginUserRequest(req *pb.LoginUserRequest) (violations []*errdetails.BadRequest_FieldViolation) {
-// 	if err := val.ValidateUsername(req.GetUsername()); err != nil {
-// 		violations = append(violations, fieldViolation("username", err))
-// 	}
+func validateLoginUserRequest(req *pb.LoginUserRequest) (violations []*errdetails.BadRequest_FieldViolation) {
+	if err := val.ValidateUsername(req.GetUsername()); err != nil {
+		violations = append(violations, fieldViolation("username", err))
+	}
 
-// 	if err := val.ValidatePassword(req.GetPassword()); err != nil {
-// 		violations = append(violations, fieldViolation("password", err))
-// 	}
+	if err := val.ValidatePassword(req.GetPassword()); err != nil {
+		violations = append(violations, fieldViolation("password", err))
+	}
 
-// 	return violations
-// }
+	return violations
+}
